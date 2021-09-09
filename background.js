5270f0b13bd7ec7a7f09e06c3942b8851a029d4a
git <turneps403@mail.ru>
2021-09-09 12:24:35 +0300

    - change playground

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
 // https://developer.chrome.com/docs/extensions/reference/action/
 chrome.action.onClicked.addListener((tab) => {
-    chrome.tabs.create({ url: "https://goplay.space", index: tab.index + 1 });
+    chrome.tabs.create({ url: "https://goplay.tools", index: tab.index + 1 });
 });
